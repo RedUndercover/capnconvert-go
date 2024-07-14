6c3b6ee0450be1b230fda4385abf979a969ad42f
Tyler Connelly <redundercover@outlook.com>
2024-07-13 21:34:59 -0400

    Add CLI for converting Go structs and interfaces to Cap'n Proto schema

---
+++ cli.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	var rootCmd = &cobra.Command{
+		Use:   "convert [file]",
+		Short: "Convert Go structs and interfaces to Cap'n Proto schema",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			goFile := args[0]
+			schema, err := Convert(goFile)
+			if err != nil {
+				fmt.Println("Error converting Go to Cap'n Proto:", err)
+				os.Exit(1)
+			}
+
+			fmt.Println(schema.String())
+		},
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
